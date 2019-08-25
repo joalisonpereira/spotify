67059711d5c7f5e8e6aa8e7e8b041e5f29ca6cab
joalisonpereira <joalison.pereira@hotmail.com>
2019-08-25 14:44:31 -0300

    Commit files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from "react-redux";
+import './config/reactotron';
+
+import GlobalStyle from './styles/global';
+import Header from './components/Header';
+import Sidebar from './components/Sidebar';
+import Player from './components/Player';
+import ErrorBox from './components/ErrorBox';
+import { Wrapper, Container, Content } from './styles/components';
+import Router from './routes';
+import store from './store';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <BrowserRouter>
+        <GlobalStyle />
+        <Wrapper>
+          <Container>
+            <Sidebar />
+            <Content>
+              <ErrorBox />
+              <Header />
+              <Router />
+            </Content>
+          </Container>
+          <Player />
+        </Wrapper>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
